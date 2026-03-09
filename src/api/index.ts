608f1f68c4d16dc161bffcecf420a768553e9a11
Md. Nuruzzaman <rajuh301@gmail.com>
2026-03-09 19:07:39 +0600

    last dd

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,5 @@
+import serverless from "serverless-http";
 import app from "../app";
 
-export default app;
+export const handler = serverless(app);
+export default handler;
--- src/server.ts
+++ src/server.ts
@@ -4,3 +4,8 @@ import config from "./config";
 app.listen(config.port, () => {
   console.log("Server running on port", config.port);
 });
+
+if (process.env.NODE_ENV !== "vercel") {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
