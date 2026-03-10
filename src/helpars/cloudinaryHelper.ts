cc378bb09074935b91fc7184069b5671f009cc4e
Md. Nuruzzaman <rajuh301@gmail.com>
2026-03-10 19:01:31 +0600

    up image

--- src/helpars/cloudinaryHelper.ts
+++ src/helpars/cloudinaryHelper.ts
@@ -2,12 +2,12 @@ import multer from "multer"
 import path from "path"
 import fs from 'fs'
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, path.join(process.cwd(), 'uploads'))
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname)
-  }
+    destination: function (req, file, cb) {
+        cb(null, path.join(process.cwd(), 'uploads'))
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
 })
 
 const upload = multer({ storage: storage })
@@ -16,31 +16,30 @@ const upload = multer({ storage: storage })
 // ------------------------------------
 import { v2 as cloudinary } from 'cloudinary';
 import { IFile } from "../app/interfaces/file"
-import config from "src/config"
 
 
 // Configuration
 cloudinary.config({
-  cloud_name: config.cloudinary.cloud_name,
-  api_key: config.cloudinary.api_key,
-  api_secret: config.cloudinary.api_secret,
+    cloud_name: 'dqtyqbfvd',
+    api_key: '497614831392694',
+    api_secret: 'CYgNockCn27VCeuaKiSm_IS6qng'  // Click 'View API Keys' above to copy your API secret
 });
 
 
 const uploadToCloudinary = async (file: IFile) => {
-  // Upload an image
-  const uploadResult = await cloudinary.uploader
-    .upload(file.path, {
-      public_id: file.originalname,
-    }
-    )
-    .catch((error) => {
-      console.log(error);
-    });
-  fs.unlinkSync(file.path)
+    // Upload an image
+    const uploadResult = await cloudinary.uploader
+        .upload(file.path, {
+            public_id: file.originalname,
+        }
+        )
+        .catch((error) => {
+            console.log(error);
+        });
+    fs.unlinkSync(file.path)
 
 
-  return uploadResult;
+    return uploadResult;
 }
 
 
@@ -50,8 +49,8 @@ const uploadToCloudinary = async (file: IFile) => {
 
 
 export const fileUploader = {
-  upload,
-  uploadToCloudinary
+    upload,
+    uploadToCloudinary
 };
 
 
